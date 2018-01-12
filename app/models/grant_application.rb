292451a80703fb1d0cc5bede0f1e8d891c65c6e7
Wynn Chen <wynn987@gmail.com>
2018-01-12 17:45:25 +0800

    added validation for grant application

--- app/models/grant_application.rb
+++ app/models/grant_application.rb
@@ -1,3 +1,18 @@
+# model for grant application
 class GrantApplication < ApplicationRecord
+  before_save { application_type.upcase }
+  VALID_APPLICANT_NAME_REGEX = /[A-Z]/i
+
   belongs_to :user
+  validates_presence_of :user
+  validates :applicant_name, presence: true, length: { maximum: 50 },
+                             format: { with: VALID_APPLICANT_NAME_REGEX }
+  validates :application_type, presence: true
+  validate :valid_application_type
+
+  def valid_application_type
+    valid_application_type_list = %w[SME MNC]
+    errors.add(:application_type, 'is invalid') unless
+      valid_application_type_list.include? application_type
+  end
 end
--- test/models/grant_application_test.rb
+++ test/models/grant_application_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class GrantApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @grant_application = GrantApplication.first
+  end
+
+  test 'should be valid' do
+    assert @grant_application.valid?
+  end
+
+  test 'applicant name should be present' do
+    @grant_application.applicant_name = ' '
+    assert_not @grant_application.valid?
+  end
+
+  test 'application type should be present' do
+    @grant_application.application_type = ' '
+    assert_not @grant_application.valid?
+  end
+
+  test 'name should not be too long' do
+    @grant_application.applicant_name = 'a' * 51
+    assert_not @grant_application.valid?
+  end
+
+  test 'application type must be valid should not be too long' do
+    @grant_application.application_type = 'aaaa'
+    assert_not @grant_application.valid?
+  end
+
+  test 'application name should not have numbers' do
+    @grant_application.applicant_name = '51'
+    assert_not @grant_application.valid?
+  end
 end
